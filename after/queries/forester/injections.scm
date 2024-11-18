df83f087060226aaa97390c1aacdc30110102ffd
utensil <utensilcandel@gmail.com>
2024-11-18 17:03:21 +0800

    Add markdown injection

--- after/queries/forester/injections.scm
+++ after/queries/forester/injections.scm
@@ -1,3 +1,11 @@
 
 ((display_math (_)+  @injection.content (#set! injection.language "latex")))
 ((inline_math (_)+  @injection.content (#set! injection.language "latex")))
+
+; Inject markdown for text nodes that follow a %md comment
+((comment) @_comment (text) @injection.content
+ (#match? @_comment "md")
+ (#set! injection.language "markdown")
+ (#not-has-ancestor? @injection.content "inline_math")
+ (#not-has-ancestor? @injection.content "display_math")
+ (#not-has-ancestor? @injection.content "code"))
